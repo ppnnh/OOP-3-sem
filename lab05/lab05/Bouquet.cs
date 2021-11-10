ae16c83aa5832736500c82fa093bf1f3dce8e53b
lerochka <76012407+ppnnh@users.noreply.github.com>
2021-11-10 18:34:35 +0300

    Add files via upload

---
+++ lab05/lab05/Bouquet.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab05
+{
+    //букет: бумага, цветок: роза, гладиолус 
+    //bouquet: paper, flower: rose, gladiolus
+    public abstract class Bouqeut
+    {
+        private int cost;
+        public virtual int Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+        public Bouqeut(int _cost)
+        {
+            cost = _cost;
+        }
+        public Bouqeut()
+        { }
+    }
+    public class Paper : Bouqeut
+    {
+        private string typeOfPaper;
+        public string TypeOfPaper
+        {
+            get { return ("Type of paper: "+typeOfPaper); }
+            set { typeOfPaper = value; }
+        }
+        public Paper(int _cost, string _typeOfPaper)
+        {
+            Cost = _cost;
+            TypeOfPaper = _typeOfPaper;
+        }
+        public Paper()
+        { }
+    }
+    public class Flower : Bouqeut
+    {
+        private int number;
+        public virtual int Number
+        { get { return number; } 
+        set { number = value; }
+        }
+        public Flower(int _cost, int _number)
+        {
+            Cost = _cost;
+            Number = _number;
+        }
+        public Flower()
+        {
+        }
+    }
+    public class Rose : Flower
+    { 
+        private string typeOfRose;
+        public string TypeOfRose
+        { get { return typeOfRose; }
+        set { typeOfRose = value; }
+        }
+        public Rose(int _cost, int _number, string _typeOfRose)
+        {
+            Cost = _cost;
+            Number = _number;
+            TypeOfRose = _typeOfRose;
+        }
+    }
+    public class Gladiolus : Flower
+    {
+        private string colour;
+        public string Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+        public Gladiolus(int _cost, int _number, string _colour)
+        {
+            Cost = _cost;
+            Number = _number;
+            Colour = _colour;
+        }
+    }
+
+}
---
+++ lab05/lab05/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Растение, Куст, Цветок, Роза, Гладиолус, Кактус, Бумага, Букет*/
+using System;
+using System.Collections.Generic;
+
+namespace lab05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bush bush1 = new Bush(1, "Forsythia", "1m", "April-May");
+            Console.WriteLine(bush1.Name);
+            Cactus cactus1 = new Cactus("Astrophytum", "15sm", "March");
+            Console.WriteLine(cactus1.Name + ", " + cactus1.MounthOfFlowering);
+            Cactus cactus2 = new Cactus();
+
+            Flower flower1 = new Flower(30, 3);
+            Rose rose1 = new Rose(30, 3, "Shrubby");
+            flower1 = rose1 as Flower;
+            if (rose1 is Flower)
+            {
+                Console.WriteLine("GetIn");
+            }
+
+            List<Plant> list = new List<Plant> { new Bush(2, ", Mahonia, ", "0.7m, ", "June"), new Cactus("Mammillaria Luti, ", "15sm, ", "July") };
+            Printer printer = new Printer();
+            foreach( Plant v in list)
+            {
+                Console.WriteLine(printer.IAmPrinting(v));
+            }
+        }
+    }
+}
